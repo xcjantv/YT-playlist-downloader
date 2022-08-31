e42a612f5694f11d69e8170ccd363c36d29da963
Jan <59826149+xcjantv@users.noreply.github.com>
2022-08-31 13:49:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import re
+import tkinter
+import tqdm
+from tkinter import filedialog
 from pytube import Playlist
+from time import sleep
+from tqdm import tqdm
+
 
 class downloader:
 
     def yt_downloader(self):
+        tkinter.Tk().withdraw()
         YOUTUBE_STREAM_AUDIO = input('Input stream ID(140 for mp3): ')  # modify the value to download a different stream
-        DOWNLOAD_DIR = input(r'Input download path: ')
+        print('Select download path:')
+        DOWNLOAD_DIR = filedialog.askdirectory()
         url = input('Input YT Playlist URL: ')
-        print(url)
         playlist = Playlist(url)
-
-        # this fixes the empty playlist.videos list
         playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+        print(len(playlist.video_urls), 'videos in playlist:')
+        #amount = len(playlist.video_urls)
+        #x=1
+        #for url in playlist.video_urls:
+            #print(url)
+        print('')
+        print('Download started...')
+        for video in tqdm(playlist.videos):
+            sleep(1)
+            print('')
+            print (video.title)
+            #print('Download started: downloading',x,'of',amount,'(',video.title,(''),')')
+            #x=x + 1
 
-        print(len(playlist.video_urls), 'videos in playlist')
-        amount = len(playlist.video_urls)
-        x=1
-        for url in playlist.video_urls:
-            print(url)
-
-        for video in playlist.videos:
-            print('Download started: downloading',x,'of',amount,'(',video,(''))
-            x=x + 1
             audioStream = video.streams.get_by_itag(YOUTUBE_STREAM_AUDIO)
-            audioStream.download(output_path=DOWNLOAD_DIR)
-        print('Download finished')
+            audioStream.download(output_path=DOWNLOAD_DIR, filename= video.title +".mp3")
 
 Downloader = downloader()
 Downloader.yt_downloader()
